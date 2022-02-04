d05c76425243d86fb55f0e27ad6a165ba34a02dc
Anji6789 <59438036+Anji6789@users.noreply.github.com>
2022-02-04 17:16:01 +0530

    Create list-ADOonprem-repositories.sh

---
+++ list-ADOonprem-repositories.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+  #read -p 'Bitbucket Username (not email): ' BB_USERNAME
+  #read -sp 'Bitbucket Password: ' BB_PASSWORD
+  #https://dev.azure.com/{organization}/_apis/git/repositories?api-version=6.0
+next_url="https://dev.azure.com/vijeths01/_apis/git/repositories?api-version=6.0"
+while [ ! -z "$next_url" ]; do
+ #   response_json=$( curl -s --user $ADO_USERNAME:$ADO_PASSWORD "$next_url" )
+  #  echo "$response_json" | jq -r '.values | map([.slug, .workspace.slug, .description, .is_private] | @csv) | join("\n")'
+   # next_url=$( echo "$response_json" | jq -r '.next' )
+#done
+test=$(curl -s --user $ADO_USERNAME:$ADO_PAT "$next_url")
+echo "$test" | jq -r '.values | map([.slug, .workspace.slug, .description, .is_private] | @csv) | join("\n")'
+next_url=$( echo "$test" | jq -r '.next' )
+done
